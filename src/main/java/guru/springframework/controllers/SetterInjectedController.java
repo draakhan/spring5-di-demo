4e76e417f39b287d92c31155a089dc8871bf9539
Bartlomiej Szymanski <B.Szymanski@kainos.com>
2019-01-11 16:08:08 +0100

    All three types of spring controller initialization are now done automatically by spring thanks to spring annotations

--- src/main/java/guru/springframework/controllers/SetterInjectedController.java
+++ src/main/java/guru/springframework/controllers/SetterInjectedController.java
@@ -2,14 +2,19 @@ package guru.springframework.controllers;
 
 import guru.springframework.services.GreetingService;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class SetterInjectedController {
     private GreetingService greetingService;
 
+    @Autowired
     public void setGreetingService(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
-    String sayHello() {
+    public String sayHello() {
         return greetingService.sayGreeting();
     }
 }
