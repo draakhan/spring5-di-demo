07cb43b2c1d90cba3d07715c12269a521fcaaf8b
Bartlomiej Szymanski <B.Szymanski@kainos.com>
2019-01-11 18:17:07 +0100

    Added a default profile

--- src/main/java/guru/springframework/services/PrimaryGermanGreetingService.java
+++ src/main/java/guru/springframework/services/PrimaryGermanGreetingService.java
@@ -6,10 +6,10 @@ import org.springframework.stereotype.Service;
 
 @Primary
 @Service
-@Profile({"en", "default"})
-public class PrimaryGreetingService implements GreetingService {
+@Profile("de")
+public class PrimaryGermanGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
-        return "Hello - Primary Greeting Service";
+        return "Primärer Grußdienst";
     }
 }
