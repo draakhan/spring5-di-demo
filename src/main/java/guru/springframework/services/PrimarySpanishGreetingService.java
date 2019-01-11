c25176465a3e827f72a728982c18c9043539bff4
Bartlomiej Szymanski <B.Szymanski@kainos.com>
2019-01-11 17:49:36 +0100

    Profiles for Spanish and English greetings added

--- src/main/java/guru/springframework/services/PrimarySpanishGreetingService.java
+++ src/main/java/guru/springframework/services/PrimarySpanishGreetingService.java
@@ -6,10 +6,10 @@ import org.springframework.stereotype.Service;
 
 @Primary
 @Service
-@Profile("en")
-public class PrimaryGreetingService implements GreetingService {
+@Profile("es")
+public class PrimarySpanishGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
-        return "Hello - Primary Greeting Service";
+        return "Saludos desde el Servicio Primario";
     }
 }
